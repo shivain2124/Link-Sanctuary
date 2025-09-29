c9b29cdf20127797d8e44e9354103f9b490f7a3b
shivain2124 <shivainsharma2124@gmail.com>
2025-09-29 23:50:17 +0530

    feat: implemented auth controllers

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,71 @@
+import { Request, Response, NextFunction } from "express";
+// import bcrypt from 'bcrypt';
+import UserModel from "../models/user.model";
+import { connectDB } from "../config/db";
+import argon2 from "argon2";
+import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
+
+//registeration logic
+export const registerController = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+
+    await connectDB();
+
+    const existingUser = await UserModel.findOne({ username });
+    if (existingUser)
+      return res.status(409).json({ message: "Username already taken" });
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered Successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: "Registration failed" });
+  }
+};
+
+//login logic
+export const loginController = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+    await connectDB();
+
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      res.status(400).json({ message: "User doesnt exists" });
+    }
+
+    const isValid = await verifyPassword(user.password, password);
+
+    if (!isValid) res.status(401).json({ message: "Invalid Credentials" });
+    res.send(201).json({ message: "Logged in Successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Login failed" });
+  }
+};
+
+// hash password with argon2
+async function hashPassword(password: string): Promise<string> {
+  try {
+    const hash = await argon2.hash(password);
+    return hash;
+  } catch (err) {
+    throw new Error("Password hashing failed");
+  }
+}
+
+// verify password
+async function verifyPassword(
+  hashFromDatabase: string,
+  submittedPassword: string
+): Promise<boolean> {
+  try {
+    const match = await argon2.verify(hashFromDatabase, submittedPassword);
+    return match;
+  } catch (err) {
+    return false;
+  }
+}
